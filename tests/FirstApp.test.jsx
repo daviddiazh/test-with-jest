9014828f5fd3a13bb5980a696f511840f96c5f9a
daviddiazh <daviddiazlds1221@gmail.com>
2022-07-09 00:50:54 -0500

    Testing nodos of DOM and elements of HTML

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -7,8 +7,23 @@ describe('Pruebas en <FirstApp /> ', () => {
     test('Debe hacer match con el snapshot', () => {
 
         const title = 'Hola, soy David'
-        render( <FirstApp title={title} /> );
+        const { container } = render( <FirstApp title={title} /> );
 
-    })
+        expect( container ).toMatchSnapshot()
+
+    });
+
+    test('El h1 debe hacer match con el snapshot', () => {
+
+        const title = 'Hola, soy David'
+        const { container, getByText } = render( <FirstApp title={title} /> );
+
+        expect( getByText(title) ).toBeTruthy() 
+        /* Con el toBeTruthy() basicamente validamos que el elemento en efecto exista*/
+
+        const h1 = container.querySelector('h1');
+        expect( h1.innerHTML ).toBe( title ); // o toContain
+
+    });
 
 })
